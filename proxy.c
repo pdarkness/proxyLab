ce40ef94d4803aa55bb0a0882babd1b8bbddf871
haukurr11 <haukurr11@skel.ru.is>
2013-03-26 22:18:04 +0000

    echoserver

--- proxy.c
+++ proxy.c
@@ -21,16 +21,44 @@ void format_log_entry(char *logstring, struct sockaddr_in *sockaddr, char *uri, 
 /* 
  * main - Main routine for the proxy program 
  */
+ echo(int connfd)
+ {
+ size_t n;
+ char buf[MAXLINE];
+ rio_t rio;
+
+ Rio_readinitb(&rio, connfd);
+ while((n = Rio_readlineb(&rio, buf, MAXLINE)) != 0) {
+ printf("server received %d bytes\n", n);
+ Rio_writen(connfd, buf, n);
+ }
+ }
+
 int main(int argc, char **argv)
 {
+ int listenfd, connfd, port, clientlen;
+ struct sockaddr_in clientaddr;
+ struct hostent *hp;
+ char *haddrp;
+ if (argc != 2) {
+    fprintf(stderr, "usage: %s <port>\n", argv[0]);
+     exit(0);
+ }
+ port = atoi(argv[1]);
+ listenfd = Open_listenfd(port);
+ while (1) {
+ clientlen = sizeof(clientaddr);
+ connfd = Accept(listenfd, (SA *)&clientaddr, &clientlen);
 
-    /* Check arguments */
-    if (argc != 2) {
-	fprintf(stderr, "Usage: %s <port number>\n", argv[0]);
-	exit(0);
-    }
-
-    exit(0);
+ /* Determine the domain name and IP address of the client */
+ hp = Gethostbyaddr((const char *)&clientaddr.sin_addr.s_addr,
+ sizeof(clientaddr.sin_addr.s_addr), AF_INET);
+ haddrp = inet_ntoa(clientaddr.sin_addr);
+ printf("server connected to %s (%s)\n", hp->h_name, haddrp);
+ echo(connfd);
+ Close(connfd);
+ }
+ exit(0);
 }
 
 
