405f24305b16a02b722d248780aa8e166b996aad
Knútur Óli <pdarkness@Darkserv.knutur.is>
2013-03-29 22:51:15 +0000

    Threading

--- proxy.c
+++ proxy.c
@@ -76,6 +76,8 @@ int main(int argc, char ** argv) {
     struct sockaddr_in clientaddr;
     struct hostent * hp;
     char * haddrp;
+    pthread_t tid;
+
     if (argc != 2) {
         fprintf(stderr, "usage: %s <port>\n", argv[0]);
         exit(0);
@@ -85,14 +87,13 @@ int main(int argc, char ** argv) {
     while (1) {
         clientlen = sizeof(clientaddr);
         connfd = Accept(listenfd, (SA * ) & clientaddr,  &clientlen);
-
         /* Determine the domain name and IP address of the client */
         hp = Gethostbyaddr((const char * ) & clientaddr.sin_addr.s_addr,
                            sizeof(clientaddr.sin_addr.s_addr), AF_INET);
         haddrp = inet_ntoa(clientaddr.sin_addr);
         printf("server connected to %s (%s)\n", hp->h_name, haddrp);
-        echo(connfd);
-        Close(connfd);
+	Pthread_create(&tid, NULL, (void *)echo, (void *)connfd);
+        //Close(connfd);
         }
     exit(0);
     }
